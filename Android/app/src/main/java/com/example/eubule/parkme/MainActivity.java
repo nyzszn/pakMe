e6a7fdf2296241e4af36ceea18852ee820cac6f8
eubule <eubulemalaba@gmail.com>
2018-01-28 10:45:23 +0300

    Adding Android code.

---
+++ Android/app/src/main/java/com/example/eubule/parkme/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.eubule.parkme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webView.loadUrl("http://192.168.1.230/pra");
+    }
+}
